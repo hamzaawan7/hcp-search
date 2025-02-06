2aa25fe8aa2092a38ca9c160b9441c39062f7f99
hamzaawan7 <saladinian@gmail.com>
2025-02-06 17:58:22 +0500

    made changes

--- backend/index.js
+++ backend/index.js
@@ -69,5 +69,5 @@ cron.schedule("*/2 * * * *", async () => {
 })();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://3.144.232.139:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
